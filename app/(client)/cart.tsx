8d8887f5982c78bb60fe70e94bb5e52d6bc75469
esarmiem <eldersarmiento1@gmail.com>
2025-04-23 20:35:39 -0500

    DELETE access-denied screen

--- app/(client)/cart.tsx
+++ app/(client)/cart.tsx
@@ -6,6 +6,8 @@ import CartCard from "~/components/epic/cartCard";
 import { useState } from "react";
 import { Stack } from "expo-router";
 import React from "react";
+import { View } from "react-native";
+import { MapPinIcon } from "lucide-react-native";
 
 export default function CartScreen() {
   const router = useRouter();
@@ -60,6 +62,20 @@ export default function CartScreen() {
       }}
     />
     <ScrollView className="h-full w-full p-4 mb-12">
+    <View className="flex-row items-center flex-1 ml-2 mb-4 border-b border-gray-400">
+          <MapPinIcon size={20} color="#ffffff" fill="#666"/>
+          <Text 
+            className="text-gray-500 text-md pr-1"
+          >
+            Enviar a
+          </Text>
+          <Text 
+            className="text-gray-500 text-md font-bold"
+            numberOfLines={1}
+          >
+            Calle 123 St 45 # 65 Sta Marta, Calle25...
+          </Text>
+        </View>
       <CartCard
         providerName="Sabores de Santa Marta"
         providerImage="https://picsum.photos/200"
